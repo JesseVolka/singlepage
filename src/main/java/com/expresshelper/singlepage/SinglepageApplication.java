ff042ba43e3d60b8a5dcfba4837dd1c7946f8a48
fanminfeng <fanminfeng@vortexinfo.cn>
2019-08-28 11:48:45 +0800

    pom文件

---
+++ src/main/java/com/expresshelper/singlepage/SinglepageApplication.java
@@ -0,0 +1,13 @@
+package com.expresshelper.singlepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglepageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinglepageApplication.class, args);
+    }
+
+}
